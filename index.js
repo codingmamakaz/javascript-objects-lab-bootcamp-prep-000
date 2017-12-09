9e0265028a1a05da960ed55fcf5fce412f98216e
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-09 20:45:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
   var newObject = Object.assign({}, object) ;
-  newObject;
-  delete newObject.key;
+  delete newObject[key];
   return newObject;
 }
